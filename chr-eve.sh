407440876d5855bb121809536a0e1819f88ac943
Ihor Hreskiv <32979134+greskiv@users.noreply.github.com>
2019-04-07 14:43:52 +0200

    Create chr-eve.sh

---
+++ chr-eve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Creating nessessary directories"
+mkdir /opt/unetlab/addons/qemu/mikrotik-6.44.2/
+cd /opt/unetlab/addons/qemu/mikrotik-6.44.2/ 
+echo "getting image of CHR"
+wget https://download.mikrotik.com/routeros/6.44.2/chr-6.44.2.img.zip
+echo "Preparing the image"
+unzip chr-6.44.2.img.zip
+rm chr-6.44.2.img.zip
+mv chr-6.44.2.img hda.qcow2
+echo "Fixing permitions of the files"
+/opt/unetlab/wrappers/unl_wrapper -a fixpermissions
