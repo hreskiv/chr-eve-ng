181baf1b328b7fa30aec38cc9cfa92f6d4ffd1dc
Ihor Hreskiv <greskiv@gmail.com>
2019-04-10 12:06:58 +0200

    Update chr-eve.sh

--- chr-eve.sh
+++ chr-eve.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
-echo "Creating nessessary directories"
-mkdir /opt/unetlab/addons/qemu/mikrotik-6.44.2/
-cd /opt/unetlab/addons/qemu/mikrotik-6.44.2/
-echo "getting image of CHR"
-wget https://download.mikrotik.com/routeros/6.44.2/chr-6.44.2.img.zip
-echo "Preparing the image"
-unzip chr-6.44.2.img.zip
-rm chr-6.44.2.img.zip
-mv chr-6.44.2.img hda.qcow2
-echo "Fixing permitions of the files"
-/opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+if [ $# -eq 1 ]; then
+    ver=$1
+    echo "Creating nessessary directories"
+    mkdir /opt/unetlab/addons/qemu/mikrotik-$ver/
+    cd /opt/unetlab/addons/qemu/mikrotik-$ver/
+    echo "getting image of CHR"
+    wget https://download.mikrotik.com/routeros/$ver/chr-$ver.img.zip
+    echo "Preparing the image"
+    unzip chr-$ver.img.zip
+    rm chr-$ver.img.zip
+    mv chr-$ver.img hda.qcow2
+    echo "Fixing permitions of the files"
+    /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+else
+    echo "You don't put any parameters."
+    echo "Usage: sh chr-eve [VERSION]"
+    echo "Where [VERSION] is the version of RouterOS"
+    echo "Sample: sh cher-eve.sh 3.44.2"
+fi
