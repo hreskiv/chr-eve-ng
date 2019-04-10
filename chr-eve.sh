459befc5b6464be9c5fb15674ac37ee3f72ca5ee
Ihor Hreskiv <greskiv@gmail.com>
2019-04-10 12:50:46 +0200

    Update chr-eve.sh

--- chr-eve.sh
+++ chr-eve.sh
@@ -4,17 +4,22 @@
 ####################################
 if [ $# -eq 1 ]; then
     ver=$1
-    echo "Creating nessessary directories"
-    mkdir /opt/unetlab/addons/qemu/mikrotik-$ver/
-    cd /opt/unetlab/addons/qemu/mikrotik-$ver/
-    echo "getting image of CHR"
-    wget https://download.mikrotik.com/routeros/$ver/chr-$ver.img.zip
-    echo "Preparing the image"
-    unzip chr-$ver.img.zip
-    rm chr-$ver.img.zip
-    mv chr-$ver.img hda.qcow2
-    echo "Fixing permitions of the files"
-    /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+    if [ -d "/opt/unetlab/addons/qemu/mikrotik-$ver/" ]; then
+	echo "The $ver of RouterOS is already installed"
+	exit 0
+    else
+        echo "Creating nessessary directories"
+        mkdir /opt/unetlab/addons/qemu/mikrotik-$ver/
+        cd /opt/unetlab/addons/qemu/mikrotik-$ver/
+        echo "getting image of CHR"
+        wget https://download.mikrotik.com/routeros/$ver/chr-$ver.img.zip
+        echo "Preparing the image"
+        unzip chr-$ver.img.zip
+        rm chr-$ver.img.zip
+        mv chr-$ver.img hda.qcow2
+        echo "Fixing permitions of the files"
+        /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+    fi;
 else
     echo "Incorrect parameters."
     echo "Usage: sh chr-eve [VERSION]"
