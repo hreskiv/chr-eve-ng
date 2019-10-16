62b8085a5d4b4ab2277c6ee2da165a769c28e3c9
Ihor Hreskiv <32979134+hreskiv@users.noreply.github.com>
2019-10-16 11:24:47 +0300

    Create chr-vultr.sh

---
+++ chr-vultr.sh
@@ -0,0 +1,22 @@
+
+curl http://download2.mikrotik.com/routeros/6.40.9/chr-6.40.9.img.zip --output chr.img.zip  && \
+yum install unzip -y && \
+gunzip -c chr.img.zip > chr.img  && \
+mount -o loop,offset=33554944 chr.img /mnt && \
+ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
+echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATEWAY
+/ip service disable telnet
+/ip service disable ftp
+/ip service disable www
+/ip service disable api
+/ip service disable api-ssl
+/user set 0 password=password2019
+ " > /mnt/rw/autorun.scr && \
+umount /mnt && \
+sleep 5 && \
+echo u > /proc/sysrq-trigger && \
+sleep 5 && \
+dd if=chr.img bs=1024 of=/dev/vda && \
+echo s > /proc/sysrq-trigger
