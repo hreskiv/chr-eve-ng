d44ae23deaf47736d57951690dc9d2ca2f35d067
Ihor Hreskiv <32979134+hreskiv@users.noreply.github.com>
2019-12-17 11:13:47 +0100

    Update chr-vultr.sh

--- chr-vultr.sh
+++ chr-vultr.sh
@@ -7,6 +7,7 @@ ADDRESS=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
 GATEWAY=`ip route list | grep default | cut -d' ' -f 3` && \
 echo "/ip address add address=$ADDRESS interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=$GATEWAY
+/ip dns set servers=8.8.8.8
 /ip service disable telnet
 /ip service disable ftp
 /ip service disable www
