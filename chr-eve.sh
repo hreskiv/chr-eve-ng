4e33c4d661b65ad3257b3bf0353539f49bc742d4
Ihor Hreskiv <32979134+hreskiv@users.noreply.github.com>
2019-12-23 12:36:22 +0100

    Update chr-eve.sh

--- chr-eve.sh
+++ chr-eve.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 ####################################
 # script is written by Ihor Hreskiv
-# version 2.2
+# version 2.4
 ####################################
 if [ $# -eq 1 ]; then
     ver=$1
-    if [ -d "/opt/unetlab/addons/qemu/mikrotik-$ver/" ]; then
-	echo "The version $ver of RouterOS is already installed"
-	exit 0
-    else
-        echo "Creating nessessary directories"
-        mkdir /opt/unetlab/addons/qemu/mikrotik-$ver/
-        cd /opt/unetlab/addons/qemu/mikrotik-$ver/
-        echo "getting image of CHR"
-        wget https://download.mikrotik.com/routeros/$ver/chr-$ver.img.zip
-        echo "Preparing the image"
-        unzip chr-$ver.img.zip
-        rm chr-$ver.img.zip
-        mv chr-$ver.img hda.qcow2
-        echo "Fixing permitions of the files"
-        /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
-    fi;
+    case $1 in
+      fix)
+          /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+          echo "Fixing permitions of the files"
+          exit 0
+          ;;
+      list)
+          ls -la /opt/unetlab/addons/qemu
+          exit 0
+          ;;
+      *)
+          if [ -d "/opt/unetlab/addons/qemu/mikrotik-$ver/" ]; then
+            echo "The version $ver of RouterOS is already installed"
+            exit 0
+          else
+            echo "Creating nessessary directories"
+            mkdir /opt/unetlab/addons/qemu/mikrotik-$ver/
+            cd /opt/unetlab/addons/qemu/mikrotik-$ver/
+            echo "getting image of CHR"
+            wget https://download.mikrotik.com/routeros/$ver/chr-$ver.img.zip
+            echo "Preparing the image"
+            unzip chr-$ver.img.zip
+            rm chr-$ver.img.zip
+            mv chr-$ver.img hda.qcow2
+            echo "Fixing permitions of the files"
+            /opt/unetlab/wrappers/unl_wrapper -a fixpermissions
+          fi;
+          ;;
+    esac
 else
     echo "Incorrect parameters."
-    echo "Usage: sh chr-eve [VERSION]"
+    echo "Usage: sh chr-eve [VERSION] | fix | list"
     echo "Where [VERSION] is the version of RouterOS"
+    echo "fix - fix the permitions of files"
+    echo "list - list of installed qemu images"
     echo "Sample: sh cher-eve.sh 6.44.3"
 fi
