97ee8d405b97244a4459d7b4e80aedd0e2806fe5
Ihor Hreskiv <32979134+hreskiv@users.noreply.github.com>
2023-03-21 08:43:39 +0100

    Update chr-ros7-do.sh

--- chr-ros7-do.sh
+++ chr-ros7-do.sh
@@ -1,11 +1,14 @@
 #########################################
-###### CHR ROS 7.5 on DigitalOcean ######
+###### CHR ROS 7.8 on DigitalOcean ######
 #########################################
 #!/bin/bash
 curl https://download.mikrotik.com/routeros/7.8/chr-7.8.img.zip --output chr.img.zip  && \
-gunzip -c chr.img.zip > chr.img 
+gunzip -c chr.img.zip > chr.img  && \
 echo u > /proc/sysrq-trigger && \
-sleep 5 && \
 dd if=chr.img bs=1024 of=/dev/vda && \
-echo s > /proc/sysrq-trigger
-
+echo "sync disk" && \
+echo s > /proc/sysrq-trigger && \
+echo "Wait 5 seconds" && \
+sleep 5 && \
+echo "All ok, I'll reboot the droplet, access to it only with reovery console" && \
+echo b > /proc/sysrq-trigger
